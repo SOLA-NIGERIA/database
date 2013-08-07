2d7e1137c07b0b8dce73e23da1c0d824439a6e5e
Islam Wali <islamwali@gmail.com>
2013-08-07 15:07:57 +0100

    modified:   migration/sola_populate_shapefiles.sql Cleaned the solapopulateshapefiles

--- migration/sola_populate_shapefiles.sql
+++ migration/sola_populate_shapefiles.sql
@@ -1,12 +1,9 @@
-﻿--INTO SOLA KANO DATABASE
+﻿--INTO SOLA KADuna DATABASE
 --modified Paola Rizzo 17 May 2013
---- additional modification by Muhammad and Dangana 21 May,2013
-
+--- 
 --interim measure to add names to imported Ward Shapefiles
 ALTER TABLE interim_data.wards DROP COLUMN IF EXISTS name;
 ALTER TABLE interim_data.wards ADD COLUMN name text;
-ALTER TABLE interim_data.wards_corrected DROP COLUMN IF EXISTS name;
-ALTER TABLE interim_data.wards_corrected ADD COLUMN name text;
 
 ----------- SPATIAL_UNIT TABLE POPULATION ----------------------------------------
 
@@ -39,16 +36,16 @@ INSERT INTO cadastre.spatial_unit_group( name,id, hierarchy_level, label,  chang
 --------------- LGA
 INSERT INTO cadastre.spatial_unit_group( id, hierarchy_level, label, name, geom, change_user) 
 	--SELECT 'KD/'||adm2, 2, adm2, 'KD/'||adm2, the_geom, 'test'
-	SELECT 'KD/'||id, 2, id, 'KD/'||id, the_geom, 'test'
+	SELECT 'KD/'||id, 2, id, 'KD/'||id, geom, 'test'
 	FROM interim_data.lga;
 	-- WHERE (ST_GeometryN(the_geom, 1) IS NOT NULL);
 
 --------------- Wards
 INSERT INTO cadastre.spatial_unit_group( id, hierarchy_level, label, name, geom, change_user, seq_nr)
-   SELECT lga_group.name || '/' ||w.ward,3, w.ward,lga_group.name || '/' ||w.ward, w.the_geom, 'test', 0
+   SELECT lga_group.name || '/' ||w.ward,3, w.ward,lga_group.name || '/' ||w.ward, w.geom, 'test', 0
    FROM cadastre.spatial_unit_group AS lga_group,  interim_data.wards AS w 
    WHERE lga_group.hierarchy_level = 2 
-   AND st_intersects(lga_group.geom, st_pointonsurface(w.the_geom));
+   AND st_intersects(lga_group.geom, st_pointonsurface(w.geom));
 
 ----------- SPATIAL_UNIT_GROUP_IN TABLE POPULATION ----------------------------------------
 
