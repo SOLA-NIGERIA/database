9219bcd9b92adebfb84861043d450d7a944462ef
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2013-09-10 16:16:37 +0200

    changed br for new parcel not overlaps existing

--- extension/business_rules.sql
+++ extension/business_rules.sql
@@ -1,27 +1,29 @@
 ﻿--- BR for not being allowed to create a new parcel which overlaps with existing ones
-delete from system.br_validation where br_id ='new-co-do-not-overlap-with-existing';
-delete from system.br_definition where br_id ='new-co-do-not-overlap-with-existing';
-delete from system.br where id ='new-co-do-not-overlap-with-existing';
+delete from system.br_validation where br_id ='new-co-must-not-overlap-with-existing';
+delete from system.br_definition where br_id ='new-co-must-not-overlap-with-existing';
+delete from system.br where id ='new-co-must-not-overlap-with-existing';
 
 insert into system.br(id, technical_type_code, feedback, technical_description) 
-values('new-co-do-not-overlap-with-existing', 'sql', 
+values('new-co-must-not-overlap-with-existing', 'sql', 
     'New polygons do not overlap with existing ones',
- '#{id} is the parameter asked. It is the transaction id.');
+ '');
 
 insert into system.br_definition(br_id, active_from, active_until, body) 
-values('new-co-do-not-overlap-with-existing', now(), 'infinity', 
-'with appo AS (select geom_polygon as pend_geom, id as pend_id from cadastre.cadastre_object  where transaction_id  =  #{id})
-select (count (co.name_firstpart)=0) as vl from cadastre.cadastre_object co, appo  where 
-id  !=  appo.pend_id and
-ST_Intersects(appo.pend_geom, co.geom_polygon)
-and co.status_code = ''current'' 
+values('new-co-must-not-overlap-with-existing', now(), 'infinity', 
+'WITH tolerance AS (SELECT CAST(ABS(LOG((CAST (vl AS NUMERIC)^2))) AS INT) AS area FROM system.setting where name = ''map-tolerance'' LIMIT 1)
+
+SELECT COALESCE(ROUND(CAST (ST_AREA(ST_UNION(co.geom_polygon))AS NUMERIC), (SELECT area FROM tolerance)) = 
+		ROUND(CAST(SUM(ST_AREA(co.geom_polygon))AS NUMERIC), (SELECT area FROM tolerance)), 
+		TRUE) AS vl
+FROM cadastre.cadastre_object co  
 ');
 
 INSERT INTO system.br_validation(br_id, target_code, target_reg_moment, target_request_type_code, severity_code, order_of_execution)
-VALUES ('new-co-do-not-overlap-with-existing', 'cadastre_object', 'current', 'cadastreChange', 'critical', 115);
+VALUES ('new-co-must-not-overlap-with-existing', 'cadastre_object', 'current', 'cadastreChange', 'critical', 115);
 
 INSERT INTO system.br_validation(br_id, target_code, target_reg_moment, target_request_type_code, severity_code, order_of_execution)
-VALUES ('new-co-do-not-overlap-with-existing', 'cadastre_object', 'pending', 'cadastreChange', 'warning', 425);
+VALUES ('new-co-must-not-overlap-with-existing', 'cadastre_object', 'pending', 'cadastreChange', 'warning', 425);
+
 -----------------------------------------------------------------------------------------------------------
 --LH # 14 DISABLE
 --target-ba_unit-check-if-pending
