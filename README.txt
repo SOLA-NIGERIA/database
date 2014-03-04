bfdd672180574c6dd4db7534722a6fbd96917ee6
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2014-03-04 14:15:28 +0100

    changeset added with scripts for updating functions and version

--- README.txt
+++ README.txt
@@ -1,8 +1,60 @@
-Database repository for the Tonga implementation of the 
-Solutions for Open Land Administration (SOLA) open source
-project. This repository contains sql scripts and database 
-backups for building and populating the SOLA PostgreSQL database.
+Database repository for the Solutions for Open Land Administration
+(SOLA) open source project. This repository contains sql scripts
+and OS batch/command files for building and populating the SOLA 
+PostgreSQL database.
 
 SOLA uses multiple Git repositories for managing its code base.
 The code repository is the super/parent repository. Refer to the 
 README in that repository for details on all SOLA Git repositories. 
+
+> Batch and Shell Command Files
+
+Batch and shell command files supported by this repository include;
+
+1)  create_database: Builds the SOLA database and loads it with 
+    configuration and test data using SQL script files located 
+    in the schema, config and data folders.
+
+
+
+> Managing Database Changes
+
+All changes to the database must be captured as a changeset script 
+in the changeset folder. The steps required when implementing 
+new database changes are;
+
+1)  Create a new ticket in Lighthouse to describe the database 
+    change that is being made. This will allocate a ticket number.
+2)  Create and run your new changeset script against your local 
+    development database to apply the change. 
+	- The changeset script must be named using the Changeset 
+      Naming Convention described below. 
+	- Any new objects added to the database (e.g. tables, columns, 
+      views, functions, etc) must include suitable database 
+      comments to describe them. 
+    - The changeset script must also update the system.version 
+	  table with the new version number implied by the changeset 
+      script file name.
+3)  Depending on the changes that were applied, run the necessary 
+    extract scripts. 
+4)  Commit changes in git and push to Github.
+
+For general development, it will not be necessary for other developers 
+to re-run any of the changeset scripts – they will just run the 
+create_database script to pick up any changes in the primary source SQL 
+files. The changeset scripts become important when it is necessary 
+to upgrade an existing production databases. In this case the DBA will 
+need to determine the changeset scripts to apply to upgrade their database 
+with new features. 
+
+> Changeset Naming Convention
+
+Scripts should be named as <year><month><sequence char>_<Lighthouse ticket 
+number>. The date fields will be based around the date the Lighthouse ticket
+is initially fixed. E.g. If Lighthouse ticket 298 is fixed on the 12th Feb 
+2014, the changeset script name is 1402a_298.sql. The next script created 
+in Feb would be 1402b_<ticket number>, etc. 
+
+The version number part of the script name is the date and sequence char 
+(e.g. 1402a). The version number must be added to the system.version 
+table when the changeset script executes. 
---
+++ changeset/1403a_9.sql
@@ -0,0 +1,139 @@
+﻿
+---
+-- Data not applicable gender
+----
+
+delete from party.gender_type where code= 'na';
+
+insert into party.gender_type(code, display_value, status) values('na', 'na', 'c');
+
+
+
+--DROP FUNCTION administrative.get_parcel_ownergender(gender character varying, query character varying);
+CREATE OR REPLACE FUNCTION administrative.get_parcel_ownergender(gender character varying, query character varying)
+RETURNS SETOF record AS
+$BODY$
+DECLARE 
+
+  rec record;
+  total decimal =0;
+  totFem decimal =0;
+  totMale decimal =0;
+  totMixed decimal =0;
+  totJoint decimal =0;
+  totEntity decimal =0;
+  totNull decimal =0;
+  parcel varchar;
+  recExt   record;
+  sqlSt varchar;
+  statusFound	boolean;
+  recToReturn	record;
+  
+BEGIN
+     total = 0;
+     sqlSt:= '';
+
+     sqlSt:= 'select sg.name   as area
+			  from  
+			  cadastre.spatial_unit_group sg 
+			  where 
+			  sg.hierarchy_level=3
+			  order by area asc
+    ';  
+    --raise exception '%',sqlSt;
+       statusFound = false;
+
+    -- Loop through results
+    
+    FOR recExt in EXECUTE sqlSt loop
+    statusFound = true; 
+     
+	for rec in 
+		SELECT 	 bu.id, 
+		(SELECT COUNT('x') FROM party.party par, administrative.party_for_rrr pfr  WHERE pfr.party_id=par.id AND rrr.id=pfr.rrr_id AND par.gender_code='female') as female,
+		(SELECT COUNT('x') FROM party.party par, administrative.party_for_rrr pfr WHERE pfr.party_id=par.id AND rrr.id=pfr.rrr_id AND par.gender_code='male') as male,
+		(SELECT COUNT('x') FROM party.party par, administrative.party_for_rrr pfr WHERE pfr.party_id=par.id AND rrr.id=pfr.rrr_id AND par.gender_code='na') as entity,
+		bu.name_lastpart  as parcel
+		FROM administrative.ba_unit_contains_spatial_unit su, application.application_property ap, 
+		application.application aa, application.service s, administrative.ba_unit bu, administrative.rrr rrr
+		WHERE bu.id=rrr.ba_unit_id
+		AND (rrr.type_code::text = 'ownership'::text 
+		     OR rrr.type_code::text = 'apartment'::text 
+		     OR rrr.type_code::text = 'commonOwnership'::text) 
+		AND (ap.ba_unit_id::text = su.ba_unit_id::text OR ap.name_lastpart::text = bu.name_lastpart::text
+		AND ap.name_firstpart::text = bu.name_firstpart::text) AND aa.id::text = ap.application_id::text 
+		AND s.application_id::text = aa.id::text AND s.request_type_code::text = 'systematicRegn'::text 
+		AND s.status_code::text = 'completed'::text  
+		AND bu.id::text = su.ba_unit_id::text 
+		AND bu.name_lastpart = ''||recExt.area||''
+		
+       loop
+
+		 if rec.female = 0 and rec.male != 0 then
+		    totMale = totMale + 1;
+		 end if;
+		 if rec.female != 0 and rec.male = 0 then
+		   totFem = totFem + 1;
+		 end if;  
+		 if rec.female = 1 and rec.male = 1 then
+		   totJoint = totJoint+1;
+		 end if;
+		 if ((rec.female > 1 and rec.male >= 1)or (rec.female >= 1 and rec.male >1)) then
+		   totMixed = totMixed+1;
+		 end if; 
+		 if ((rec.female = 0 and rec.male = 0) and rec.entity >0) then
+		     totEntity =  totEntity + 1;
+		 end if;
+		 if (rec.female = 0 and rec.male = 0 and rec.entity =0) then
+		     totNull =  totNull + 1;
+		 end if;
+		 total := totMale+totFem+totJoint+totMixed+totEntity+totNull;
+        end loop; 
+        
+
+          parcel = recExt.area;
+                   
+	  select into recToReturn
+	        parcel::    varchar,
+                total::     decimal,
+		totFem::    decimal,
+		totMale::   decimal,
+		totMixed::  decimal,
+		totJoint::  decimal,
+		totEntity:: decimal,
+		totNull::   decimal;
+		                         
+           return next recToReturn;
+          statusFound = true;
+          total     =0;
+	  totFem    =0;
+	  totMale   =0;
+	  totMixed  =0;
+	  totJoint  =0;
+	  totEntity =0;
+	  totNull   =0;
+    end loop;
+   
+    if (not statusFound) then
+         parcel = 'none';
+                
+        select into recToReturn
+	       	parcel::   varchar,
+                total::    decimal,
+		totFem::   decimal,
+		totMale::  decimal,
+		totMixed:: decimal,
+		totJoint:: decimal,
+		totEntity:: decimal,
+		totNull::   decimal;
+		                         
+		                         
+          return next recToReturn;
+
+    end if;
+    return;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION administrative.get_parcel_ownergender(character varying,character varying) OWNER TO postgres;
