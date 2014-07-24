d6efbd04ad74f0d35d60f99f2ef95d6a9df8d420
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2014-07-24 18:06:29 +0200

    new create db script

--- create_soladb_nigeria.bat
+++ create_soladb_nigeria.bat
@@ -9,7 +9,7 @@ set username=postgres
 set archive_password=?
 set label=Enter State (Kano,Ondo,Kaduna,Kogi,Jigawa,CrossRiver...)
 
-set lga=Enter LGA
+set lga=Enter LGA  (Ungogo, Fagge, AKR, Kaduna, LKJ, KAB....)
 
 set createDB=NO
 
@@ -44,9 +44,22 @@ echo
 echo Starting Build at %time%
 echo Starting Build at %time% > build.log 2>&1
 
+
+REM echo the files in the folder state/changeset
+for /f "eol=: delims=" %%F in (
+  'dir ..\database-%label%\%changesetPath%\*.sql /b /a-d /one   2^>nul'
+) do echo  %%F 
+
+REM echo the files in the folder changeset
+for /f "eol=: delims=" %%F in (
+  'dir %changesetPath%\*.sql /b /a-d /one   2^>nul'
+) do echo  %%F 
+
+
+
 echo Creating database...
 echo Creating database... >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=sola.sql > build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=sola.sql >> build.log 2>&1
 
 echo Loading business rules...
 echo Loading SOLA business rules... >> build.log 2>&1
@@ -88,68 +101,22 @@ echo Loading   User Roles... >> build.log 2>&1
 %psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%extensionPath%users_roles.sql >> build.log 2>&1
 echo Loading   LGA and Ward Boundaries... >> build.log 2>&1
 %psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=..\database-%label%\%migrationPath%sola_populate_shapefiles.sql >> build.log 2>&1
+echo Loading   Systematic Registration Reports... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%extensionPath%create_generator_nr.sql >> build.log 2>&1
 
-
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%1-1403a_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%2-1403a_10.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%3-1403a_11.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%4-1403a_6.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%5-1403a_9.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%6-1403b_6.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%7-1403b_9.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%8-1403b_39.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%9-1403b_49.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%10-1403b_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%11-1403c_50.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%12-1403c_51.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%13-1403c_52.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%14-1403c_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%15-1403d_10.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%16-1403d_53.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%17-1403d_54.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%18-1403d_55.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%19-1403d_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%20-1404a_58.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%21-1404a_62.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%22-1404a_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%23-1404b_64.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%24-1404b_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%25-1404c_64.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%26-1404c_65.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%27-1404c_66.sql >> build.log 2>&1
 echo INPUT LGA: %lga% >> ..\build.log 2>&1
 %psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --command="select system.set_system_id('%lga%')" >> build.log 2>&1
 
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%28-1404c_67.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%29-1404c_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%30-1404d_24.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%31-1404d_55.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%32-1404d_70.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%33-1404d_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%34-1405a_65.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%35-1405a_74.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%36-1405a_70.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%37-1405a_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%38-1405b_76.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%39-1405b_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%40-1405c_78.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%41-1405c_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%42-1405c_76.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%43-1405d_79.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%44-1406a_80.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%45-1406a_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%46-1406b_82.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%47-1406b_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%48-1406c_85.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%49-1407a_86.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%50-1407a_8.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%51-1407b_85.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%52-1407b_87.sql >> build.log 2>&1
+REM echo the files in the folder changeset
+for /f "eol=: delims=" %%F in (
+  'dir %changesetPath%\*.sql /b /a-d /one   2^>nul'
+) do %psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%changesetPath%\%%F  >> build.log 2>&1
 
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=..\database-%label%\%changesetPath%1-1405a_GR.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=..\database-%label%\%changesetPath%2-1405c_MD.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=..\database-%label%\%changesetPath%3-1407a_MD.sql >> build.log 2>&1
-%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=..\database-%label%\%changesetPath%4-1407a_MD.sql >> build.log 2>&1
+
+for /f "eol=: delims=" %%F in (
+  'dir ..\database-%label%\%changesetPath%\*.sql /b /a-d /one   2^>nul'
+) do %psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=..\database-%label%\%changesetPath%\%%F  >> build.log 2>&1
+
 
 echo Finished at %time% - Check build.log for errors!
 echo Finished at %time% >> build.log 2>&1
