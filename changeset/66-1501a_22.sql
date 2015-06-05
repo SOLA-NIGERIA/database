fb8d953b70b159ffa079282c6d11941893734df0
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2015-06-05 14:02:48 +0200

    daily work unit search

--- changeset/66-1501a_22.sql
+++ changeset/66-1501a_22.sql
@@ -9,25 +9,25 @@
 
 -- DROP VIEW cadastre.hierarchy;
 
---CREATE OR REPLACE VIEW cadastre.hierarchy AS 
- --SELECT sug.id, sug.label, sug.geom, 
-   -- sug.hierarchy_level::character varying AS filter_category
-   --FROM cadastre.spatial_unit_group sug;
+CREATE OR REPLACE VIEW cadastre.hierarchy AS 
+SELECT sug.id, sug.label, sug.geom, 
+    sug.hierarchy_level::character varying AS filter_category
+   FROM cadastre.spatial_unit_group sug;
 
---ALTER TABLE cadastre.hierarchy
-  ---OWNER TO postgres;
---COMMENT ON VIEW cadastre.hierarchy
-  --IS 'First (highest) level of the hierarchical spatial unit group object of a hierarchical structure such as administrative rights.';
-
-
-
---INSERT INTO system.query (name, sql, description) VALUES ('SpatialResult.getHierarchy', 'select id, label, st_asewkb(geom) as the_geom, filter_category  from cadastre.hierarchy where ST_Intersects(geom, ST_SetSRID(ST_3DMakeBox(ST_Point(#{minx}, #{miny}),ST_Point(#{maxx}, #{maxy})), #{srid})) and st_area(geom)> power(5 * #{pixel_res}, 2)', 'Query is used from Spatial Unit Group Editor to edit hierarchy records');
---INSERT INTO system.config_map_layer (name, title, type_code, active, visible_in_start, item_order, style, url, wms_layers, wms_version, wms_format, wms_data_source, pojo_structure, pojo_query_name, pojo_query_name_for_select, shape_location, security_user, security_password, added_from_bulk_operation, use_in_public_display) VALUES ('sug_hierarchy', 'Hierarchy', 'pojo', true, false, 9, 'sug-hierarchy.xml', NULL, NULL, NULL, NULL, NULL, 'theGeom:Polygon,label:"",filter_category', 'SpatialResult.getHierarchy', NULL, NULL, NULL, NULL, false, true);
+ALTER TABLE cadastre.hierarchy
+  OWNER TO postgres;
+COMMENT ON VIEW cadastre.hierarchy
+  IS 'First (highest) level of the hierarchical spatial unit group object of a hierarchical structure such as administrative rights.';
 
 
 
+INSERT INTO system.query (name, sql, description) VALUES ('SpatialResult.getHierarchy', 'select id, label, st_asewkb(geom) as the_geom, filter_category  from cadastre.hierarchy where ST_Intersects(geom, ST_SetSRID(ST_3DMakeBox(ST_Point(#{minx}, #{miny}),ST_Point(#{maxx}, #{maxy})), #{srid})) and st_area(geom)> power(5 * #{pixel_res}, 2)', 'Query is used from Spatial Unit Group Editor to edit hierarchy records');
+INSERT INTO system.config_map_layer (name, title, type_code, active, visible_in_start, item_order, style, url, wms_layers, wms_version, wms_format, wms_data_source, pojo_structure, pojo_query_name, pojo_query_name_for_select, shape_location, security_user, security_password, added_from_bulk_operation, use_in_public_display) VALUES ('sug_hierarchy', 'Hierarchy', 'pojo', true, false, 9, 'sug-hierarchy.xml', NULL, NULL, NULL, NULL, NULL, 'theGeom:Polygon,label:"",filter_category', 'SpatialResult.getHierarchy', NULL, NULL, NULL, NULL, false, true);
 
---INSERT INTO system.br (id, display_name, technical_type_code, feedback, description, technical_description) VALUES ('spatial-unit-group-inside-other-spatial-unit-group', 'spatial-unit-group-inside-other-spatial-unit-group', 'sql', 'Spatial unit groups that are not of the top hierarchy must be spatially inside another spatial unit group with hierarchy which is a level up. Tolerance of 0.5 m is applied.::::Пространственные группы, которые не находятся на самой вершине иерархии, должны быть расположены внутри других пространственных групп с более высшим уровнем иерархии. Погрешность может составлять 0.5 м.', NULL, 'There is no parameter required.');
+
+
+
+INSERT INTO system.br (id, display_name, technical_type_code, feedback, description, technical_description) VALUES ('spatial-unit-group-inside-other-spatial-unit-group', 'spatial-unit-group-inside-other-spatial-unit-group', 'sql', 'Spatial unit groups that are not of the top hierarchy must be spatially inside another spatial unit group with hierarchy which is a level up. Tolerance of 0.5 m is applied.::::Пространственные группы, которые не находятся на самой вершине иерархии, должны быть расположены внутри других пространственных групп с более высшим уровнем иерархии. Погрешность может составлять 0.5 м.', NULL, 'There is no parameter required.');
 
 INSERT INTO system.br_definition (br_id, active_from, active_until, body) VALUES ('spatial-unit-group-inside-other-spatial-unit-group', '2014-02-20', 'infinity', 'select count(*)= 0 as vl
 from cadastre.spatial_unit_group 
---
+++ changeset/68-1506a_xx.sql
@@ -0,0 +1,11 @@
+﻿insert into system.query(name, sql) values('map_search.cadastre_object_by_dailyworkunit', 'select sg.id, sg.label, st_asewkb(sg.geom) as the_geom from  
+cadastre.spatial_unit_group sg 
+where compare_strings(#{search_string}, sg.name) 
+and sg.hierarchy_level=5
+limit 30');
+
+insert into system.map_search_option(code, title, query_name, active, min_search_str_len, zoom_in_buffer) 
+values('DAILYWORKUNIT', 'Daily Work Unit', 'map_search.cadastre_object_by_dailyworkunit', true, 3, 50);
+
+
+
